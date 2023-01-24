3ae2489314002dad010d6c31a3cf31e5da9f9b0f
Marilyn Thai <marilynthai15@gmail.com>
2023-01-24 15:15:11 -0800

    Initial Commit

---
+++ NewTestProject/NewTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTestProjectApp.swift
+//  NewTestProject
+//
+//  Created by Marilyn Thai on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct NewTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
