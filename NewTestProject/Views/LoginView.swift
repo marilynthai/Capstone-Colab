2d3ec0a31e00aa1e3cfab3c69e932b598073a2c8
nina-tuyen tran <ninatuyentran@gmail.com>
2023-01-25 14:36:18 -0800

    Created a login page with textfield for username and secure field for password, created a function to validate the user info based on hardcoded dev data

--- NewTestProject/Views/LoginView.swift
+++ NewTestProject/Views/LoginView.swift
@@ -8,10 +8,71 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Color.blue
+                    .opacity(0.25)
+                    .ignoresSafeArea()
+                VStack {
+                    Text("TBD App Name")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width:300, height:50)
+                        .background(Color.white.opacity(0.75))
+                        .cornerRadius(10)
+                        .border(.red,width: CGFloat(wrongUsername))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width:300, height:50)
+                        .background(Color.white.opacity(0.75))
+                        .cornerRadius(10)
+                        .border(.red,width: CGFloat(wrongPassword))
+                    
+                    
+                    Button("Login") {
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.black)
+                    .frame(width:300, height:50)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You have successfully logged in!"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
+            }
+            .navigationBarHidden(true)
+        }
     }
-}
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "nina" {
+            wrongUsername = 0
+            if password.lowercased() == "abc123" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
+            }
+        }
+    }
+    
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
