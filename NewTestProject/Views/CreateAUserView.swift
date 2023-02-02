c39ca2b9f5ee3a3dbfa7e47c3e901d65f8a89dd2
Marilyn Thai <marilynthai15@gmail.com>
2023-02-02 09:00:38 -0800

    Fixed create a User to navigate back to home.

--- NewTestProject/Views/CreateAUserView.swift
+++ NewTestProject/Views/CreateAUserView.swift
@@ -22,74 +22,80 @@ struct CreateAUserView: View {
         NavigationView{
             ZStack {
                 VStack{
-                    Text("Create A New User")
-                        .font(.largeTitle)
-                        .bold()
-                        .padding()
-                    
-                    TextField("First Name", text: $firstName)
-                        .padding()
-                        .frame(width:300, height:50)
-                        .background(Color.white.opacity(0.75))
-                        .cornerRadius(10)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                        .textInputAutocapitalization(.never)
-
-                    
-                    TextField("Last Name", text: $lastName)
-                        .padding()
-                        .frame(width:300, height:50)
-                        .background(Color.white.opacity(0.75))
-                        .cornerRadius(10)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                        .textInputAutocapitalization(.never)
-
-                    
-                    TextField("Email", text: $email)
-                        .padding()
-                        .frame(width:300, height:50)
-                        .background(Color.white.opacity(0.75))
-                        .cornerRadius(10)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                        .textInputAutocapitalization(.never)
-
-                    
-                    TextField("Password", text: $password)
-                        .padding()
-                        .frame(width:300, height:50)
-                        .background(Color.white.opacity(0.75))
-                        .cornerRadius(10)
-                        .buttonBorderShape(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=shape: ButtonBorderShape@*/.automatic/*@END_MENU_TOKEN@*/)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                        .textInputAutocapitalization(.never)
-
-                    
-                    
-                    Button("Sign Up") {
-                        register()
-                        dataManager.addUser(name: firstName ,email: email)
-                        createdUser = true
-                        email = ""
-                        password = ""
+                    if createdUser != true {
+                        Text("Create A New User")
+                            .font(.largeTitle)
+                            .bold()
+                            .padding()
                         
+                        TextField("First Name", text: $firstName)
+                            .padding()
+                            .frame(width:300, height:50)
+                            .background(Color.white.opacity(0.75))
+                            .cornerRadius(10)
+                            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                            .textInputAutocapitalization(.never)
+                        
+                        
+                        TextField("Last Name", text: $lastName)
+                            .padding()
+                            .frame(width:300, height:50)
+                            .background(Color.white.opacity(0.75))
+                            .cornerRadius(10)
+                            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                            .textInputAutocapitalization(.never)
+                        
+                        
+                        TextField("Email", text: $email)
+                            .padding()
+                            .frame(width:300, height:50)
+                            .background(Color.white.opacity(0.75))
+                            .cornerRadius(10)
+                            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                            .textInputAutocapitalization(.never)
+                        
+                        
+                        TextField("Password", text: $password)
+                            .padding()
+                            .frame(width:300, height:50)
+                            .background(Color.white.opacity(0.75))
+                            .cornerRadius(10)
+                            .buttonBorderShape(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=shape: ButtonBorderShape@*/.automatic/*@END_MENU_TOKEN@*/)
+                            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                            .textInputAutocapitalization(.never)
+                        
+                        
+                        
+                        Button("Sign Up") {
+                            register()
+                            dataManager.addUser(name: firstName ,email: email)
+                            createdUser = true
+                            email = ""
+                            password = ""
+                            firstName=""
+                            lastName=""
+                            
+                        }
+                        .foregroundColor(.black)
+                        .frame(width:300, height:50)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        
+                    } else {
+                        HomeView()
                     }
-                    .foregroundColor(.black)
-                    .frame(width:300, height:50)
-                    .background(Color.white)
-                    .cornerRadius(10)
+//                    NavigationLink(destination: HomeView()) {
+//                        Text("Login")
+//                    }
+//                    .foregroundColor(.black)
+//                    .frame(width:300, height:50)
+//                    .background(Color.white)
+//                    .cornerRadius(10)
                     
-                    
-                    NavigationLink(destination: LoginView()) {
-                        Text("Login")
-                    }
-                    .foregroundColor(.black)
-                    .frame(width:300, height:50)
-                    .background(Color.white)
-                    .cornerRadius(10)
-                    
-                    NavigationLink(destination:Text("You have successfully created an account!"), isActive: $createdUser) {
-                        EmptyView()
-                    }
+//                    NavigationLink("", destination:  HomeView(), isActive: $createdUser)
+//                    NavigationLink(destination:Text("You have successfully created an account!"), isActive: $createdUser) {
+//                        EmptyView()
+//                    }
                     
 //
 //                    Button("Login") {
